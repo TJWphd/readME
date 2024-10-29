a740f879014f72fbe79d0fe63017115666ca96f0
“Thomas <tjwhitephd@gmail.com>
2024-10-28 17:37:40 -0700

    added questions with help from classmate Alex Mena

--- index.js
+++ index.js
@@ -3,6 +3,45 @@
 // TODO: Create an array of questions for user input
 const questions = [];
 
+const inquire = require("inquirer");
+const fs = require("fs");
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "questionONe",
+      name: "var1",
+    },
+    {
+      type: "input",
+      message: "question2?",
+      name: "var2",
+    },
+    {
+      type: "list",
+      message: "question3?",
+      name: "var3",
+      choices: ["x", "y", "z"],
+    },
+    {
+      type: "input",
+      message: "question4?",
+      name: "var4",
+    },
+  ])
+  .then((response) => {
+    console.log(
+      "Your logo will be ",
+      response.text,
+      response.textColor,
+      response.shape,
+      response.shapeColor,
+      "!"
+    );
+    writeToFile(response);
+  });
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
